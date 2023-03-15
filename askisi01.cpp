eaefcbe4992e08e33271f7ca58cb0ac00acd0ad8
YAGUAR68diek <126086766+YAGUAR68diek@users.noreply.github.com>
2023-03-15 16:10:12 +0200

    Lisi problem 2

---
+++ askisi01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int X, Y;
+cout<<"Enter first number";
+cin>>Y;
+cout<<"Entersecond number";
+cin>>X;
+cout<<"Athroisma is: "<<(X+Y)<<endl<<"To Givomevo is: "<<(X*Y)<<endl;
+}
