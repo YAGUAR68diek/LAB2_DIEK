e18b5a17507249549c75f686a33d688559c21980
YAGUAR68diek <126086766+YAGUAR68diek@users.noreply.github.com>
2023-03-15 17:08:12 +0200

    OggosSfairas

---
+++ OggosSfaira_Lysn.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<iostream>
+#define pi 3.14
+using namespace std;
+int main(){
+float r;
+cout<<"dose aktiva sfairas"<<endl;
+cin>>r;
+cout<<"o ogos tis sfairas me aktina: "<<endl;
+cout<<(r)<<endl<<(4*pi*r*r*r/3)<<endl;
+}
